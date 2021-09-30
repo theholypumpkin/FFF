a5bdb070d22b2384abef79a809219dc2ec8eeea9
Pascal S <49909487+theholypumpkin@users.noreply.github.com>
2021-09-30 15:23:06 +0200

    Version 2.0

--- code.py
+++ code.py
@@ -15,22 +15,26 @@ from adafruit_display_text.label import Label
 import adafruit_ccs811
 import adafruit_dht
 import adafruit_ina219  # Ampere / Volt Sensor
+import multichannel_gas_sensor
 # -----------------------------------------------------------------------------
 # Setting up Sensors
 i2c = busio.I2C(board.GP1, board.GP0)
 ccs = adafruit_ccs811.CCS811(i2c)
 dht = adafruit_dht.DHT22(board.GP5)
-ina_battery = adafruit_ina219.INA219(i2c)
-ina_solar = adafruit_ina219.INA219(i2c, 0x41)
+ina_battery = adafruit_ina219.INA219(i2c, 0x41)
+nox_co = multichannel_gas_sensor.GM_Multi_Gas(i2c)
+# ina_solar = adafruit_ina219.INA219(i2c, 0x41)
 
 switch = Switch()
 
 # Global Variables
 image_cycle = 0
 i = 0  # TODO set value to 0
-solar_power = []
+# solar_power = []
 power_usage = []
 co2_concentration = []
+co_concentration = []
+nox_concentration = []
 temperature = []
 humidity = []
 # -----------------------------------------------------------------------------
@@ -120,7 +124,8 @@ def scroll(line, x, y=0, iterations=1):
 def read_sensors():
     # TODO Try-Catch-block!
     power_usage.append(ina_battery.power)  # Read the power usage every tile
-    solar_power.append(ina_solar.power)  # Read power production on every til
+    print(f"{ina_battery.power}W")
+    #solar_power.append(ina_solar.power)  # Read power production on every til
     try:
         co2_concentration.append(ccs.eco2)
     except RuntimeError as e:
@@ -128,13 +133,17 @@ def read_sensors():
         co2_concentration.append(450)
     else:
         print("co2 concentration appended to list")
+
+    nox_concentration.append(nox_co.measureNO2())
+    co_concentration.append(nox_co.measureCO())
+    print("co and nox concentration appended to respective lists")
     try:
         temperature.append(dht.temperature)
         humidity.append(dht.humidity)
     except RuntimeError as e:
         print("Execption: ", e.args)  # When the checksum did not validated
-        temperature.append(25)
-        humidity.append(50)
+        temperature.append(20) # TODO adjust to tomorrows avg temperature
+        humidity.append(85) # TODO adjust to tomorrows avg humidity
     else:
         print("temperature and humidity appended to list")
 # -----------------------------------------------------------------------------
@@ -147,144 +156,143 @@ def tile_line_values(line, text="", color=0x000000,
     line.x = x
     line.y = y
 # -----------------------------------------------------------------------------
-# the 1.3 Degrees Earth Tiles
-def tile_0():
-    read_sensors()
-    tile_line_values(line2)  # Reset lines
-    tile_line_values(line3)
-    tile_line_values(line1, "how much?", 0x0000ff, x=64, y=8)
-    scroll(line1, x=-1, iterations=119)
-# -----------------------------------------------------------------------------
-# the 1.5 Degrees Earth Tiles
-def tile_1():
-    read_sensors()
-    tile_line_values(line1, "Stop!", 0xff0000, x=64, y=8)
-    scroll(line1, x=-1, iterations=95)
-# -----------------------------------------------------------------------------
-# The two degrees Earth tile
-def tile_2():
-    read_sensors()
-    tile_line_values(line1, " TO HOT!", 0xffffff, 0xff0000, x=64, y=7)
-    scroll(line1, x=-1, iterations=112)
-# -----------------------------------------------------------------------------
-# The 4 Degree Earth tile
-def tile_3():
-    read_sensors()
-    tile_line_values(line1, "End of Socity!",
-                     0xff0000, x=64, y=8)
-    scroll(line1, x=-1, iterations=145)
-# -----------------------------------------------------------------------------
-# All tile which don't have text
-def tile_4_9_A_B_10_11():
-    read_sensors()
-# -----------------------------------------------------------------------------
 # The tile Show your Stripes
-def tile_5():
+def tile_0():
     read_sensors()
     tile_line_values(line1)  # Reset line1
     tile_line_values(line2, "#ShowYourStripes", 0xff0000, x=63, y=24)
     scroll(line2, x=-1, iterations=158)
 # -----------------------------------------------------------------------------
 # The tile displaying the Sun and Battery
-def tile_6():
+def tile_1():
     # We display the mean (avarage) of all power_usage readings of last 2 min
     read_sensors()
-    print(solar_power)
+    #print(solar_power)
     print(power_usage)
-    tile_line_values(line1, "{:.2} W".format(sum(solar_power)/len(solar_power)),
-                     0xffffff, 0x000000, x=20, y=8)
+    #tile_line_values(line1, "{:.2} W".format(sum(solar_power)/len(solar_power)),
+                    # 0xffffff, 0x000000, x=20, y=8)
     tile_line_values(line2, "{:.2} W".format(sum(power_usage)/len(power_usage)),
-                     0xffffff, 0x000000, x=20, y=24)
-    solar_power.clear()  # Remove all values from array so we can fill it again
+                     0xffffff, 0x000000, x=20, y=16)
+    # solar_power.clear()  # Remove all values from array so we can fill it again
     power_usage.clear()
 # -----------------------------------------------------------------------------
 # The tile displaying the Green Gost
-def tile_7():
+def tile_2():
     read_sensors()
-    tile_line_values(line1, "Buhh!", 0x16ca31, x=32, y=6)
-    tile_line_values(line2, "Bring", 0x16ca31, x=32, y=16)
-    tile_line_values(line3, "Oko!", 0x16ca31, x=32, y=26)
+    tile_line_values(line1, "Buhh!", 0x16ca31, x=32, y=5)
+    tile_line_values(line2, "Bring", 0x16ca31, x=32, y=15)
+    tile_line_values(line3, "Oko!", 0x16ca31, x=32, y=25)
 # -----------------------------------------------------------------------------
-# The tile displaying the Smokestacks and CO2
-def tile_8():
+# The tile displaying the Smokestacks and Carbon Monoxide Levels
+def tile_3():
+    read_sensors()
+    print(co_concentration)
+    tile_line_values(line2)  # Reset Lines
+    tile_line_values(line3)
+    # We display the avarage CO2 concentration of the last two minutes
+    try:
+        tile_line_values(line1,
+                         f'CO {int(sum(co_concentration)/len(co_concentration))}',
+                         0xff0000, x=16, y=8)
+    except ZeroDivisionError:
+        print("ZeroDivisionError")
+    #tile_line_values(line1, "CO: 150", 0xff0000, x=20, y=10)
+    tile_line_values(line2, "ppb", 0xff0000, x=40, y=20)
+    co_concentration.clear()
+# -----------------------------------------------------------------------------
+# Fuck SPD, Fuck CDU
+def tile_4():
+    read_sensors()
+    tile_line_values(line1, "SPD", 0xff0000, x=8, y=4)
+    tile_line_values(line2, "&", 0xffffff, x=30, y=4)
+    tile_line_values(line3, "CDU", 0x0000ff, x=40, y=4)
+    time.sleep(10)
+    #TODO Scrolling or have two lines which are displayed after oneother
+# -----------------------------------------------------------------------------
+# The Temperature tile
+def tile_5():
+    read_sensors()
+    print(temperature)
+    # Print the temperature with on foating point number
+    tile_line_values(line1, "{:.2} Grad".format(sum(temperature)/len(temperature)),
+                     0xffff00, x=12, y=6)
+
+    tile_line_values(line2, "Celsius",
+                     0xffff00, x=12, y=16)
+    temperature.clear()
+# -----------------------------------------------------------------------------
+# The CDU is Poop tile has no text
+def tile_6_10():
+    read_sensors()
+# -----------------------------------------------------------------------------
+#
+def tile_7():
     read_sensors()
     print(co2_concentration)
     tile_line_values(line2)  # Reset Lines
     tile_line_values(line3)
     # We display the avarage CO2 concentration of the last two minutes
-    tile_line_values(line1,
-                     f'CO2 {int(sum(co2_concentration)/len(co2_concentration))}',
-                     0xff0000, x=16, y=8)
-    tile_line_values(line2, "PPM", 0xff0000, x=40, y=20)
+    tile_line_values(line2,
+                     f'{int(sum(co2_concentration)/len(co2_concentration))} ppm',
+                     0xff0000, x=2, y=25)
+    tile_line_values(line1, "CO2:", 0xff0000, x=2, y=16)
     co2_concentration.clear()
 # -----------------------------------------------------------------------------
-# The tile displaying the Explosion
-def tile_C():
+# No more highways
+def tile_8():
     read_sensors()
-    tile_line_values(line1, "NOW!", 0xff0000, x=20, y=14)
+    tile_line_values(line1, "Keine A-49", 0xffffff, x=2, y=4)
 # -----------------------------------------------------------------------------
-# The CDU Tiles
-def tile_D_E_F():
+# The tile displaying the Nitrogendioxide concentration
+def tile_9():
     read_sensors()
-    tile_line_values(line1, "Gibt", 0xffffff, x=4, y=4)
-    tile_line_values(line2, "mir", 0xffffff, x=4, y=16)
-    tile_line_values(line3, "ein", 0xffffff, x=4, y=27)
+    try:
+        tile_line_values(line1, f'{int(sum(nox_concentration)/len(nox_concentration))}',
+                         0xffffff, x=32, y=12)
+    except ZeroDivisionError:
+        print("ZeroDivisionError")
+    # tile_line_values(line1, "20", 0xffffff, x=40, y=12)
+    nox_concentration.clear()
 # -----------------------------------------------------------------------------
 # The Temperature, humidity tiles
-def tile_12():
+def tile_11():
     read_sensors()
-    print(temperature)
     print(humidity)
     # Print the temperature with on foating point number
-    tile_line_values(line1, "{:.2}".format(sum(temperature)/len(temperature)),
-                     0xffff00, x=12, y=6)
+    tile_line_values(line1, "Humidity:",
+                     0x0000ff, x=4, y=8)
 
     tile_line_values(line2, f'{int(sum(humidity)/len(humidity))}%',
                      0x0000ff, x=32, y=24)
-    temperature.clear()
+    #TODO scrolling
     humidity.clear()
-# -----------------------------------------------------------------------------
-# The black empty tile with github link
-def tile_13():
-    tile_line_values(line1, "Smash Ca(r)pitalism", 0x0000ff, x=64, y=8)
-    scroll(line1, x=-1, iterations=179)
 # -----------------------------------------------------------------------------
 # Add an case for each tile
 case_tile_0 = tile_0
 case_tile_1 = tile_1
 case_tile_2 = tile_2
 case_tile_3 = tile_3
-case_tile_4_9_A_B_10_11 = tile_4_9_A_B_10_11
+case_tile_4 = tile_4
 case_tile_5 = tile_5
-case_tile_6 = tile_6
+case_tile_6_10 = tile_6_10
 case_tile_7 = tile_7
 case_tile_8 = tile_8
-case_tile_C = tile_C
-case_tile_D_E_F = tile_D_E_F
-case_tile_12 = tile_12
-case_tile_13 = tile_13
+case_tile_9 = tile_9
+case_tile_11 = tile_11
 
 # adding cases to switch
 switch.add_case(0, case_tile_0, True)
 switch.add_case(1, case_tile_1, True)
 switch.add_case(2, case_tile_2, True)
 switch.add_case(3, case_tile_3, True)
-switch.add_case(4, case_tile_4_9_A_B_10_11, True)
+switch.add_case(4, case_tile_4, True)
 switch.add_case(5, case_tile_5, True)
-switch.add_case(6, case_tile_6, True)
+switch.add_case(6, case_tile_6_10, True)
 switch.add_case(7, case_tile_7, True)
 switch.add_case(8, case_tile_8, True)
-switch.add_case(9, case_tile_4_9_A_B_10_11, True)
-switch.add_case(10, case_tile_4_9_A_B_10_11, True)
-switch.add_case(11, case_tile_4_9_A_B_10_11, True)
-switch.add_case(12, case_tile_C, True)
-switch.add_case(13, case_tile_D_E_F, True)
-switch.add_case(14, case_tile_D_E_F, True)
-switch.add_case(15, case_tile_D_E_F, True)
-switch.add_case(16, case_tile_4_9_A_B_10_11, True)
-switch.add_case(17, case_tile_4_9_A_B_10_11, True)
-switch.add_case(18, case_tile_12, True)
-switch.add_case(19, case_tile_13, True)
+switch.add_case(9, case_tile_9, True)
+switch.add_case(10, case_tile_6_10, True)
+switch.add_case(11, case_tile_11, True)
 
 # -----------------------------------------------------------------------------
 # Wait for the sensor to be ready
@@ -296,16 +304,18 @@ while True:
         bitmap_tiles[0] = i
         sub = time.monotonic()  # get the time before the execution of switch
         switch.case(i)
+        i += 1
         # substract the execution time of switch from the sleep time
         dif = time.monotonic() - sub
-        print(f'dif before: {dif}')
+        # print(f'dif before: {dif}')
         if dif > 5:  # When a tile takes longer than 5 sec to process
             dif = 0
         else:
             dif = 5 - dif  # when tile takes less than 5 sec to process
-        i += 1 # Load the next tile
+        # print(f'difafter:{dif}')
+
         image_cycle = time.monotonic()
-        if (i > 19):
+        if (i > 11):
             i = 0
     time.sleep(dif)
     tile_line_values(line1)  # Reset the line Label
